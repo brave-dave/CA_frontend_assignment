a1f84d26f653d7bd3fb15ef92afdcabdc1aea1b1
Davide Bruno <davidebruno1989@gmail.com>
2021-08-12 15:16:18 +0200

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,26 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import CharactersListPage from "./pages/CharactersListPage";
 import ErrorPage from "./pages/ErrorPage";
+import { Provider } from "react-redux";
+import store from "./redux";
 
 ReactDOM.render(
   <BrowserRouter>
-    <AppLayout>
-      <Switch>
-        <Route path="/" exact>
-          <CharactersListPage />
-        </Route>
-        <Route path="/page-:page">
-          <CharactersListPage />
-        </Route>
-        <Route path="*">
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </AppLayout>
+    <Provider store={store}>
+      <AppLayout>
+        <Switch>
+          <Route path="/" exact>
+            <CharactersListPage />
+          </Route>
+          <Route path="/page-:page">
+            <CharactersListPage />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </AppLayout>
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
