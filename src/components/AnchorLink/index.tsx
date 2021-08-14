21b714cbd49f4048275f20118e4f6a28114690e7
Davide Bruno <davidebruno1989@gmail.com>
2021-08-14 12:55:55 +0200

    Correct anchor link

--- src/components/AnchorLink/index.tsx
+++ src/components/AnchorLink/index.tsx
@@ -2,20 +2,18 @@ import {
   Link as ReactRouterLink,
   LinkProps as ReactRouterLinkProps,
 } from "react-router-dom";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 interface LinkProps extends Omit<ReactRouterLinkProps, "to"> {
   to: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    link: {
-      color: "inherit",
-      textDecoration: "inherit",
-    },
-  })
-);
+const useStyles = makeStyles({
+  link: {
+    color: "inherit",
+    textDecoration: "inherit",
+  },
+});
 
 export default function AnchorLink(props: LinkProps) {
   const classes = useStyles();
